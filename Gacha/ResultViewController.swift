18109c20f635b52d5ad65f847c0e876659fd22cf
NorichikaSugii <wawa60379@gmail.com>
2023-05-08 16:27:41 +0900

    Initial Commit

---
+++ Gacha/ResultViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ResultViewController.swift
+//  Gacha
+//
+//  Created by 杉井位次 on 2023/05/08.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    
+    var number: Int!
+    
+    @IBOutlet var backgroundImageView: UIImageView!
+    @IBOutlet var characterImageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
